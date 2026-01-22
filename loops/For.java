0ecf16f89cec33a7aa0b2a05026f39d4b75e3c3d
DAVANPALLY KOWSHIK RAO <kowshikraod2005@gmail.com>
2026-01-22 08:38:12 +0530

    Add files via upload

---
+++ loops/For.java
@@ -0,0 +1,7 @@
+public class For {
+  public static void main(String[] args) {
+        for(int counter=0;counter <=10;counter ++){
+            System.out.println(counter+" ");
+        }
+    }
+}
